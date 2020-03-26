0603a012ff96378f28d0daf139f8a313c20f28ab
Eliezer Croitoru <ngtech1ltd@gmail.com>
2020-03-26 23:50:53 +0000

    Upgrading the block page and build script

--- build-all.sh
+++ build-all.sh
@@ -8,6 +8,8 @@ VERSION_NUMBER="1.1.41"
 
 BUILD_ARRAY=`ls -d redwood-*/`
 
+NOCACHE=$(cat no-cache)
+
 if [[ ! -z "${BUILD_ONLY}" ]];then
 	BUILD_ARRAY=`ls -d */|egrep "${BUILD_ONLY}"`
 fi
@@ -19,8 +21,8 @@ do
 	cd "$Build"
 	DOCKER_IMAGE=`cat dockerimage`
 	echo "${DOCKER_IMAGE}"
-	stat dockerimage && \
-		( docker image inspect  "${DOCKER_IMAGE}" && docker build . -t "${DOCKER_IMAGE}" )
+	stat build && stat dockerimage && \
+		 docker build  . -t "${DOCKER_IMAGE}" ${NOCACHE} && \
 	stat build && docker run -it \
 	        -e COMPRESSION="${COMPRESSION}" \
 	        -e DEBUG_MODE="${DEBUG_MODE}" \
